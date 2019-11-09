bd7d291fe1565383d3097e385efa734880ee0862
josh <critesjosh@gmail.com>
2019-11-09 11:29:07 -0500

    update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.5.0;
- 
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
- 
-contract token is ERC20Mintable {
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/GSN/GSNRecipient.sol";
+
+contract Token is GSNRecipient, ERC20Mintable, ERC20Burnable {
   constructor() public {
   }
 }
